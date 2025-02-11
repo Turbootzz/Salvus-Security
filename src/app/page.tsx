51428d778beda6d83acf1061abc6576b7a28dc38
Turbootzz <tc.herman@hotmail.com>
2025-02-11 20:54:57 +0100

    changing header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Header from "../components/layout/Header";
 
 export default function Home() {
   return (
 	<>
+	<Header />
 	</>
 	);
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,12 @@
+import Phone from '@/components/common/icons/Phone';
+
 export default function Header() {
-	return (
-		<header>
-			<div className=""></div>
-		</header>
-	)
+  return (
+	<header>
+	  <div className="bg-dark-blue_c p-4 text-center">
+		<Phone />
+		030 - 123 45 67
+	  </div>
+	</header>
+  );
 }
